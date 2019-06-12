7f45bc219cd51c9ebd2c65faa4554d08dbcd34ca
Caio Corrêa Chaves <caio.chaves@etec.sp.gov.br>
2019-06-12 15:03:08 -0300

    Comentários de planejamento base adicionados

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <gui.h>
+#include <logica.h>
 
 int main(void)
 {
+    // Mostrar tela de escolha do tamanho do tabuleiro
+    // Captar regiao do clique e gerar tabuleiro (Parte lógica)
+    // Mostrar tabuleiro
     
-
+    // [Ciclo]
+    // Captar clique
+    // Processar clique (Parte lógica)
+    // Saída
+    
+    // [Em caso de erros]
+    // Criar arquivo para log (Parte lógica)
+    
+    // [Ao fim do jogo]
+	// No caso de quebra de records, salvar dados do jogador num ranking e 
+	//	dados da partida
+	
     return 0;
 }
